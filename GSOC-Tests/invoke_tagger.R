923e4ed91fc87ac39f7dbfcaa0b1b6c4eb50889c
shubh24 <shubhankar24@gmail.com>
2016-03-11 14:42:12 +0530

    Maxent Tagger - Task 2!

---
+++ GSOC-Tests/invoke_tagger.R
@@ -0,0 +1,15 @@
+library(openNLP)
+library(NLP)
+
+invoke_tagger = function(text){
+  
+  sentence_tokenizer <- Maxent_Sent_Token_Annotator()
+  word_tokenizer <- Maxent_Word_Token_Annotator()
+  pos_tag_tokenizer <- Maxent_POS_Tag_Annotator(probs = TRUE)
+  
+  tokenizers <- annotate(text, list(sentence_tokenizer, word_tokenizer))
+  pos_tags_with_prob = annotate(text, pos_tag_tokenizer, tokenizers)
+  
+  print(pos_tags_with_prob)
+  
+}
