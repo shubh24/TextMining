8173de7525821f19d3f5cb313358a3d1e9c1a3b5
shubh24 <shubhankar24@gmail.com>
2016-03-22 15:37:35 +0530

    Missing source import

--- GSOC-Tests/get_text.R
+++ GSOC-Tests/get_text.R
@@ -1,7 +1,8 @@
+source("./functions.R")
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args) != 2) {
-  stop("Give appropriate arguments.")
+  stop("First Argument - File Name. Second Argument - URL")
 } else if (length(args) == 2){
   url = args[2]
   f = args[1]
